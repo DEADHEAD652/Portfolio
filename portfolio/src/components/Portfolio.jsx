5754ba09e91fc6c17cdadaa3b6bf593b39bec56a
DEADHEAD652 <hamzajjmalik@gmail.com>
2023-04-11 20:48:10 +0500

    portfolio

---
+++ portfolio/src/components/Portfolio.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import feedback from "../assets/portfolio/feedback.png";
+import gym from "../assets/portfolio/gym.png";
+import hTube from "../assets/portfolio/H tube.png";
+
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: feedback,
+      link: 'https://rainbow-faun-772bb4.netlify.app/'
+    },
+
+    {
+      id: 2,
+      src: gym,
+      link: 'https://hamza-gym.netlify.app/'
+
+    },
+
+    {
+      id: 3,
+      src: hTube,
+      link: 'https://h-media.netlify.app/'
+
+    },
+  ];
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8 ">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6 text-gray-400">Check out some of my work</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className="flex  items-center justify-center">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Demo
+                </button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Code
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
